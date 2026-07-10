db20c80cc305b006b3727222a1bef22f89249559
JoseAntonioAcebedoAragon <168141674+JoseAntonioAcebedoAragon@users.noreply.github.com>
2026-07-10 19:57:47 +0200

    Update AuthController.java

--- backend/src/main/java/com/saniikos/backend/controllers/AuthController.java
+++ backend/src/main/java/com/saniikos/backend/controllers/AuthController.java
@@ -1,5 +1,38 @@
 package com.saniikos.backend.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.saniikos.backend.authorization.LoginRequestDTO;
+import com.saniikos.backend.authorization.LoginResponseDTO;
+import com.saniikos.backend.services.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
 public class AuthController {
-    
+
+    private final AuthService authService;
+
+
+    public AuthController(
+            AuthService authService) {
+
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(
+            @Valid @RequestBody LoginRequestDTO request) {
+
+
+        LoginResponseDTO response =
+                authService.login(request);
+
+
+        return ResponseEntity.ok(response);
+    }
+
 }
