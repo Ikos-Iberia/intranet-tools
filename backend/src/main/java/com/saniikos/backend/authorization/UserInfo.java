6db03a8a99077b7ed0864b276b23a28941c4cbf8
JoseAntonioAcebedoAragon <168141674+JoseAntonioAcebedoAragon@users.noreply.github.com>
2026-07-10 19:57:42 +0200

    Update UserInfo.java

--- backend/src/main/java/com/saniikos/backend/authorization/UserInfo.java
+++ backend/src/main/java/com/saniikos/backend/authorization/UserInfo.java
@@ -1,25 +1,86 @@
 package com.saniikos.backend.authorization;
 
+import java.util.Collection;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserInfo {
+@JsonIgnoreProperties({
+        "authorities",
+        "accountNonExpired",
+        "accountNonLocked",
+        "credentialsNonExpired",
+        "enabled"
+})
+public class UserInfo implements UserDetails {
 
-	private Long id;
+    private Long id;
 
-	private String username;
+    private String username;
 
-	@JsonIgnore
-	private String password;
+    private String password;
 
-	private Set<String> roles;
+    private Set<String> roles;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+
+    @Override
+    public String getPassword() {
+
+        return password;
+    }
+
+
+    @Override
+    public String getUsername() {
+
+        return username;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked() {
+
+        return true;
+    }
+
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
 
 }
