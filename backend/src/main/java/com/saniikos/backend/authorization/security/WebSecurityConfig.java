d5d61bd3c51bbee489b98491a95aa23cafff6c20
JoseAntonioAcebedoAragon <168141674+JoseAntonioAcebedoAragon@users.noreply.github.com>
2026-07-10 18:38:08 +0200

    Update WebSecurityConfig.java

--- backend/src/main/java/com/saniikos/backend/authorization/security/WebSecurityConfig.java
+++ backend/src/main/java/com/saniikos/backend/authorization/security/WebSecurityConfig.java
@@ -76,6 +76,7 @@ public class WebSecurityConfig {
         authWhitelist.add("/api/tool-permissions/**");
         authWhitelist.add("/api/users/**");
         authWhitelist.add("/api/user-roles/**");
+        authWhitelist.add("/api/user-activities/**");
         // TODO Revisar esto, ya que solo /users/login deberia ser publico, el resto
         // deberia requerir autenticacion y permisos de usuario
 
