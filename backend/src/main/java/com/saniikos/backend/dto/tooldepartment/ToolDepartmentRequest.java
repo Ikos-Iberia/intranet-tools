d7607555debf9d179a884e55eaa4de7d230dae40
JoseAntonioAcebedoAragon <168141674+JoseAntonioAcebedoAragon@users.noreply.github.com>
2026-07-11 13:09:36 +0200

    Update ToolDepartmentRequest.java

--- backend/src/main/java/com/saniikos/backend/dto/tooldepartment/ToolDepartmentRequest.java
+++ backend/src/main/java/com/saniikos/backend/dto/tooldepartment/ToolDepartmentRequest.java
@@ -6,15 +6,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Tool department request")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ToolDepartmentRequest {
 
+    @Schema(description = "Tool id", example = "1")
     @NotNull
     private Long toolId;
 
+    @Schema(description = "Department id", example = "1")
     @NotNull
     private Long departmentId;
 
