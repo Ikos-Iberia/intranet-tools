d1f7f09bcfafe2749f12a20f8f5d1344c35bcd49
JoseAntonioAcebedoAragon <168141674+JoseAntonioAcebedoAragon@users.noreply.github.com>
2026-07-11 13:09:59 +0200

    Update UserRoleRequest.java

--- backend/src/main/java/com/saniikos/backend/dto/userrole/UserRoleRequest.java
+++ backend/src/main/java/com/saniikos/backend/dto/userrole/UserRoleRequest.java
@@ -6,15 +6,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+@Schema(description = "User role request")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRoleRequest {
 
+    @Schema(description = "User id", example = "1")
     @NotNull
     private Long userId;
 
+    @Schema(description = "Role id", example = "1")
     @NotNull
     private Long roleId;
 
