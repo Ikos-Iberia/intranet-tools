8effcbcd82d210eb2c649afed9e29f84876c2959
JoseAntonioAcebedoAragon <168141674+JoseAntonioAcebedoAragon@users.noreply.github.com>
2026-07-11 13:09:41 +0200

    Update ToolPermissionRequest.java

--- backend/src/main/java/com/saniikos/backend/dto/toolpermission/ToolPermissionRequest.java
+++ backend/src/main/java/com/saniikos/backend/dto/toolpermission/ToolPermissionRequest.java
@@ -6,15 +6,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+@Schema(description = "Tool permission request")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ToolPermissionRequest {
 
+    @Schema(description = "Tool id", example = "1")
     @NotNull
     private Long toolId;
 
+    @Schema(description = "Role id", example = "1")
     @NotNull
     private Long roleId;
 
